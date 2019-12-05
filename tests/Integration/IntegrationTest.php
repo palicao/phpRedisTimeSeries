a166ce6e9b24846d038001d526b5b9f528da4707
Milos Zivanovic <dzeparac@yahoo.com>
2019-12-05 21:54:51 +0100

    fix date time rounding issue for range and multirange

--- tests/Integration/IntegrationTest.php
+++ tests/Integration/IntegrationTest.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 
 namespace Palicao\PhpRedisTimeSeries\Tests\Integration;
 
+use DateInterval;
+use DateTime;
 use DateTimeImmutable;
 use Palicao\PhpRedisTimeSeries\AggregationRule;
+use Palicao\PhpRedisTimeSeries\DateTimeUtils;
 use Palicao\PhpRedisTimeSeries\Filter;
 use Palicao\PhpRedisTimeSeries\Label;
 use Palicao\PhpRedisTimeSeries\RedisClient;
@@ -150,4 +153,64 @@ class IntegrationTest extends TestCase
         $this->assertEquals($expectedResult, $range);
     }
 
+    public function testAddAndRetrieveWithDateTimeObjectAsMultiRangeWithMultipleFilters(): void
+    {
+        $currentDate = new DateTime();
+        $from = $currentDate->sub(new DateInterval('P1D'));
+        $to = $currentDate;
+
+        $this->sut->create(
+            'temperature:3:11',
+            6000,
+            [new Label('sensor_id', '2'), new Label('area_id', '32')]
+        );
+        $this->sut->add(new Sample('temperature:3:11', 30, $from));
+        $this->sut->add(new Sample('temperature:3:11', 42, $to));
+
+        $filter = new Filter('sensor_id', '2');
+        $filter->add('area_id', Filter::OP_EQUALS, '32');
+
+        $range = $this->sut->multiRange($filter);
+
+        $expectedRange = [
+            Sample::createFromTimestamp('temperature:3:11', (float)42, DateTimeUtils::timestampWithMsFromDateTime(new DateTimeImmutable($to->format('Y-m-d H:i:s.u'))))
+        ];
+
+        $this->assertEquals($expectedRange, $range);
+    }
+
+
+    public function testAddAndRetrieveWithDateTimeObjectAsRange(): void
+    {
+        $from = new DateTimeImmutable('2019-11-06 20:34:17.103000');
+        $to = new DateTimeImmutable('2019-11-06 20:34:17.107000');
+
+        $this->sut->create(
+            'temperature:3:11',
+            null,
+            [new Label('sensor_id', '2'), new Label('area_id', '32')]
+        );
+
+        $this->sut->add(new Sample('temperature:3:11', 30, $from));
+        $this->sut->add(new Sample('temperature:3:11', 42, $to));
+
+        $range = $this->sut->range(
+            'temperature:3:11'
+        );
+
+        $expectedRange = [
+            Sample::createFromTimestamp(
+                'temperature:3:11',
+                (float)30,
+                DateTimeUtils::timestampWithMsFromDateTime(new DateTimeImmutable($from->format('Y-m-d H:i:s.u')))
+            ),
+            Sample::createFromTimestamp(
+                'temperature:3:11',
+                (float)42,
+                DateTimeUtils::timestampWithMsFromDateTime(new DateTimeImmutable($to->format('Y-m-d H:i:s.u')))
+            ),
+        ];
+
+        $this->assertEquals($expectedRange, $range);
+    }
 }

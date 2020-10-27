ca53e7a1e207be925b3f84e0aa5539404a586957
Alessandro Balasco <palicao@gmail.com>
2020-10-27 22:07:41 +0100

    remove package-versions and fix psalm warning (#20)

--- src/DateTimeUtils.php
+++ src/DateTimeUtils.php
@@ -5,12 +5,17 @@ namespace Palicao\PhpRedisTimeSeries;
 
 use DateTimeImmutable;
 use DateTimeInterface;
+use Palicao\PhpRedisTimeSeries\Exception\TimestampParsingException;
 
 class DateTimeUtils
 {
     public static function dateTimeFromTimestampWithMs(int $timestamp) : DateTimeInterface
     {
-        return DateTimeImmutable::createFromFormat('U.u', sprintf('%.03f', $timestamp / 1000));
+        $dateTime = DateTimeImmutable::createFromFormat('U.u', sprintf('%.03f', $timestamp / 1000));
+        if ($dateTime === false) {
+            throw new TimestampParsingException(sprintf("Unable to parse timestamp: %d", $timestamp));
+        }
+        return $dateTime;
     }
 
     public static function timestampWithMsFromDateTime(DateTimeInterface $dateTime) : int
---
+++ src/Exception/TimestampParsingException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Palicao\PhpRedisTimeSeries\Exception;
+
+use InvalidArgumentException;
+
+class TimestampParsingException extends InvalidArgumentException
+{
+}

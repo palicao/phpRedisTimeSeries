4bdc6a72033467cf6b777a8049fad97d5378a241
alessandro <alessandro.balasco@oetkerdigital.com>
2021-07-11 09:51:22 +0200

    Fixes #19 and improves coverage

--- tests/Unit/RedisClientTest.php
+++ tests/Unit/RedisClientTest.php
@@ -72,4 +72,45 @@ class RedisClientTest extends TestCase
         $sut = new RedisClient($redisMock, $connectionParams);
         $sut->executeCommand(['MY', 'command']);
     }
+
+    public function testConnectionWithPassword(): void
+    {
+        $redisMock = $this->createMock(Redis::class);
+        $redisMock->expects(self::once())->method('isConnected')->willReturn(false);
+        $redisMock->expects(self::once())->method('connect')->with(
+            '127.0.0.1',
+            6379,
+            0,
+            0,
+            0.0
+        );
+        $redisMock->expects(self::once())
+            ->method('auth')
+            ->with('pass');
+        $connectionParams = new RedisConnectionParams('127.0.0.1', 6379, null, 'pass');
+        $sut = new RedisClient($redisMock, $connectionParams);
+        $sut->executeCommand(['MY', 'command']);
+    }
+
+    public function testConnectionWithUseranameAndPassword(): void
+    {
+        $redisMock = $this->createMock(Redis::class);
+        $redisMock->expects(self::once())->method('isConnected')->willReturn(false);
+        $redisMock->expects(self::once())->method('connect')->with(
+            '127.0.0.1',
+            6379,
+            0,
+            0,
+            0.0
+        );
+        $redisMock->expects(self::exactly(2))
+            ->method('rawCommand')
+            ->withConsecutive(
+                ['AUTH', 'username', 'pass'],
+                ['MY', 'command']
+            );
+        $connectionParams = new RedisConnectionParams('127.0.0.1', 6379, 'username', 'pass');
+        $sut = new RedisClient($redisMock, $connectionParams);
+        $sut->executeCommand(['MY', 'command']);
+    }
 }
